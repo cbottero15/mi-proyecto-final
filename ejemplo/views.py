850534d67b1e2b22558ab03cec6aab1c9e2d4231
Claudio Bottero <claudiobottero1@gmail.com>
2022-10-12 18:43:38 -0300

    ejemplo de clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -3,5 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+
+    suma = 5 + 10
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre":"Claudio",
+        "apellido": suma,
+        
+        })
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html",
+    {
+        "nombre":nombre,
+        "apellido":apellido,
+
+    })
+
+def index_tres(request):
     
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8]}
+    )
+
+    def imc(request, peso, altura):
+        imc = 1
+        return render(request, "ejemplo/imc.html", {"imc":imc"})
+        
