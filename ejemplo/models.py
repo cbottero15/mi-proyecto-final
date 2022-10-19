dc681f9850f5684c85554d2ce315efdfa96d34d0
drvsoft <diegorubenvaira@live.com>
2022-10-19 12:39:47 -0300

    Agregue campos a la tabla y cambie el modelo

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Familiar(models.Model):
-      nombre = models.CharField(max_length=100)
-      direccion = models.CharField(max_length=200)
+      apellido = models.CharField(max_length=30)
+      nombre = models.CharField(max_length=40)
+      direccion = models.CharField(max_length=60)
+      localidad = models.CharField(max_length=40)
       numero_pasaporte = models.IntegerField()
 
 def __str__(self):
-      return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
+      return f"{self.apellido}, {self.nombre}, {self.localidad}, {self.id}"
